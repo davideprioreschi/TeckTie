2dd0c19cefcc0c0bf7a1eb796137028000fce47f
davideprioreschi <105057661+davideprioreschi@users.noreply.github.com>
2023-12-14 15:45:55 +0100

    0.1.5

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-window.onscroll = function () {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 300) {
-        document.getElementById("tornaSuBtn").style.display = "block";
-    } else {
-        document.getElementById("tornaSuBtn").style.display = "none";
-    }
-};
-document.getElementById("tornaSuBtn").onclick = function () {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-};
+// window.onscroll = function () {
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 300) {
+//         document.getElementById("tornaSuBtn").style.display = "block";
+//     } else {
+//         document.getElementById("tornaSuBtn").style.display = "none";
+//     }
+// };
+// document.getElementById("tornaSuBtn").onclick = function () {
+//     window.scrollTo({ top: 0, behavior: 'smooth' });
+// };
 
